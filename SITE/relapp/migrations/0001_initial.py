0ac6fb7d321deabe6a567e329557547b944606e0
Олег <bonbarcovoleg55@gmail.com>
2024-11-01 12:39:00 +0300

    update migration

--- SITE/relapp/migrations/0001_initial.py
+++ SITE/relapp/migrations/0001_initial.py
@@ -25,4 +25,16 @@ class Migration(migrations.Migration):
                 'verbose_name_plural': 'Клиенты',
             },
         ),
-       
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=255, verbose_name='Заголовок')),
+                ('content', models.TextField(blank=True, verbose_name='Текст статьи')),
+                ('photo', models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')),
+                ('time_create', models.DateTimeField(auto_now_add=True, verbose_name='Время создания')),
+                ('time_update', models.DateTimeField(auto_now=True, verbose_name='Время изменения')),
+                ('is_published', models.BooleanField(default=True, verbose_name='Публикация')),
+                ('category', models.CharField(default='Продукты', max_length=100, verbose_name='Категория')),
+            ],
+         
